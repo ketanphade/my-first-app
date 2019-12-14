20b96d9c77200ebd9a48951f8f3b66338ad79009
ketan.ak24 <ketan.ak24@gmail.com>
2019-12-14 20:08:08 +0530

    Initial commit

---
+++ src/main/java/com/sins/ash/AshApplication.java
@@ -0,0 +1,13 @@
+package com.sins.ash;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AshApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AshApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sins/ash/AshApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sins.ash;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AshApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
