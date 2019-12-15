6f5fedffcf394b2a8d896de36cd1ec31c2713a21
ketan.ak24 <ketan.ak24@gmail.com>
2019-12-15 15:23:49 +0530

    Modified session controller

--- src/main/java/com/sins/ash/controller/SessionController.java
+++ src/main/java/com/sins/ash/controller/SessionController.java
@@ -1,4 +1,35 @@
 package com.sins.ash.controller;
 
+import com.sins.ash.models.Session;
+import com.sins.ash.repository.SessionRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/v1/sessions")
 public class SessionController {
+
+    @Autowired
+    private SessionRepository sessionRepository;
+
+    @GetMapping
+    public List<Session> list(){
+        return sessionRepository.findAll();
+    }
+
+    @GetMapping
+    @RequestMapping({"id"})
+    public Session get(@PathVariable Long id){
+        return sessionRepository.getOne(id);
+    }
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public Session create(@RequestBody final Session session){
+        return sessionRepository.saveAndFlush(session);
+    }
+
 }
