bda08987f29b555989ce663e642b555d8bdaa9f0
ketan.ak24 <ketan.ak24@gmail.com>
2019-12-15 15:40:23 +0530

    Modified Speaker COntroller

--- src/main/java/com/sins/ash/controller/SpeakerController.java
+++ src/main/java/com/sins/ash/controller/SpeakerController.java
@@ -1,4 +1,40 @@
 package com.sins.ash.controller;
 
+import com.sins.ash.models.Speaker;
+import com.sins.ash.repository.SpeakerRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/v1/speakers")
 public class SpeakerController {
+
+    @Autowired
+    SpeakerRepository speakerRepository;
+
+    @GetMapping
+    public List<Speaker> speakers(){
+        return speakerRepository.findAll();
+    }
+
+    @GetMapping
+    @RequestMapping({"id"})
+    public Speaker get(@PathVariable Long id){
+        return speakerRepository.getOne(id);
+    }
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public Speaker create(@RequestBody final Speaker speaker){
+        return speakerRepository.saveAndFlush(speaker);
+    }
+
+
+
+
+
+
 }
