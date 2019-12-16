ab64657f306794498a957d1843789b4ecc10c59a
ketan.ak24 <ketan.ak24@gmail.com>
2019-12-16 16:14:42 +0530

    Added JsonIgnoreProperties

--- src/main/java/com/sins/ash/models/Session.java
+++ src/main/java/com/sins/ash/models/Session.java
@@ -1,9 +1,12 @@
 package com.sins.ash.models;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
 import javax.persistence.*;
 import java.util.List;
 
 @Entity(name = "Sessions")
+@JsonIgnoreProperties({"hibernateLazyInitializer","handler"})
 public class Session {
 
     @Id
