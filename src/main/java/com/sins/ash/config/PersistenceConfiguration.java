f96881ddb51c87c47f0a85c653f437256dd1558d
ketan.ak24 <ketan.ak24@gmail.com>
2019-12-17 15:53:25 +0530

    Created Persistence Configuration file

---
+++ src/main/java/com/sins/ash/config/PersistenceConfiguration.java
@@ -0,0 +1,19 @@
+package com.sins.ash.config;
+
+import org.springframework.boot.jdbc.DataSourceBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import javax.sql.DataSource;
+
+@Configuration
+public class PersistenceConfiguration {
+
+    @Bean
+    public DataSource dataSource(){
+        DataSourceBuilder builder= DataSourceBuilder.create();
+        builder.url("jdbc:postgresql://localhost:7841/postgres");
+        System.out.println("My custom datasource is initialized");
+        return builder.build();
+    }
+}
