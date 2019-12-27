67c2802bcf5e1b532f700168d383bafcf009f2fa
ketanphade <ketan.ak24@gmail.com>
2019-12-27 18:00:20 +0530

    Modified docker and commented persistence config

--- src/main/java/com/sins/ash/config/PersistenceConfiguration.java
+++ src/main/java/com/sins/ash/config/PersistenceConfiguration.java
@@ -8,15 +8,14 @@ import javax.sql.DataSource;
 
 @Configuration
 public class PersistenceConfiguration {
-
-/*    @Bean
-    public DataSource dataSource(){
-        DataSourceBuilder builder= DataSourceBuilder.create();
-        builder.url("jdbc:postgresql://localhost:5432/postgres");
-        builder.username("postgres");
-        builder.password("sam");
-        System.out.println("My custom datasource is initialized");
-        return builder.build();
-    }
-*/
+//
+//    @Bean
+//    public DataSource dataSource(){
+//        DataSourceBuilder builder= DataSourceBuilder.create();
+//        builder.url("jdbc:postgresql://localhost:5432/postgres");
+//        builder.username("postgres");
+//        builder.password("sam");
+//        System.out.println("My custom datasource is initialized");
+//        return builder.build();
+//    }
 }
